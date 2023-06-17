55e8716d3757d419454dc24835f6c8b9e1f5c6fb
Vladimir Gladkov (office-win10) <vovkos@gmail.com>
2023-06-17 17:50:56 +0800

    [app] updates re latest changes in axl, graco

--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -70,9 +70,7 @@ Lexer::createDoxyCommentToken(int tokenKind) {
 		ASSERT(false);
 	}
 
-	Token* token = createStringToken(tokenKind, left, right);
-	token->m_channelMask = TokenChannelMask_DoxyComment;
-	return token;
+	return createStringToken(tokenKind, left, right);
 }
 
 //..............................................................................
